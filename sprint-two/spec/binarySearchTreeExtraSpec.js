b5aa5001071d2f64227cf51e3432745871a162dd
tgrrtt <jtgarrett123@gmail.com>
2014-09-13 10:58:08 -0700

    Binary search tree can rebalance

--- sprint-two/spec/binarySearchTreeExtraSpec.js
+++ sprint-two/spec/binarySearchTreeExtraSpec.js
@@ -28,6 +28,28 @@ describe('binarySearchTree++', function() {
     expect(binarySearchTree.contains(8)).to.equal(false);
   });
 
+  it('should have a working "size" method', function(){
+    binarySearchTree.insert(7);
+    binarySearchTree.insert(2);
+    binarySearchTree.insert(3);
+    binarySearchTree.insert(1);
+    binarySearchTree.insert(10);
+    binarySearchTree.insert(11);
+    expect(binarySearchTree.size()).to.equal(7);
+  });
+
+  it('should have a working "sortedList" method', function(){
+    binarySearchTree.insert(7);
+    binarySearchTree.insert(2);
+    binarySearchTree.insert(3);
+    binarySearchTree.insert(1);
+    binarySearchTree.insert(10);
+    binarySearchTree.insert(11);
+
+    var sorted = binarySearchTree.toSortedList()
+    expect(sorted).to.eql([1,2,3,5,7,10,11]);
+  });
+
   it('should execute a callback on every value in a tree using "depthFirstLog"', function(){
     var array = [];
     var func = function(value){ array.push(value); };
@@ -38,7 +60,8 @@ describe('binarySearchTree++', function() {
     expect(array).to.eql([5,2,3]);
   });
 
-  it('should rebalance the tree when maximum depth is greater than twice minimum depth', function() {
+  it('should rebalance the tree', function() {
+    binarySearchTree = makeBinarySearchTree(0);
     binarySearchTree.insert(1);
     binarySearchTree.insert(2);
     binarySearchTree.insert(3);
@@ -48,7 +71,22 @@ describe('binarySearchTree++', function() {
     binarySearchTree.insert(7);
     binarySearchTree.insert(8);
     binarySearchTree.insert(9);
-    expect(binarySearchTree.minDepth()).to.equal(4);
-    expect(binarySearchTree.maxDepth()).to.equal(9);
+    binarySearchTree.rebalance();
+    expect(binarySearchTree.size()).to.equal(10);
+  });
+
+  it('should rebalance the tree when maximum depth is greater than twice minimum depth', function() {
+    binarySearchTree = makeBinarySearchTree(0);
+    binarySearchTree.insert(1);
+    binarySearchTree.insert(2);
+    binarySearchTree.insert(3);
+    binarySearchTree.insert(4);
+    binarySearchTree.insert(5);
+    binarySearchTree.insert(6);
+    binarySearchTree.insert(7);
+    binarySearchTree.insert(8);
+    binarySearchTree.insert(9);
+    expect(binarySearchTree.value).to.equal(5);
+    expect(binarySearchTree.size()).to.equal(10);
   });
 });
--- sprint-two/src/binarySearchTreeExtra.js
+++ sprint-two/src/binarySearchTreeExtra.js
@@ -7,22 +7,81 @@ var makeBinarySearchTree = function(value){
   return newTree;
 };
 
+
 makeBinarySearchTree.prototype = {
   insert: function(value) {
+    function traverse (tree) {
+      if (tree.value < value) {
+        if (!tree.right) {
+          tree.right = makeBinarySearchTree(value);
+        } else {
+          traverse(tree.right);
+        }
+      } else if (tree.value > value) {
+        if (!tree.left) {
+          tree.left = makeBinarySearchTree(value);
+        } else {
+          traverse(tree.left);
+        }
+      } else {
+        return;
+      }
+    }
+    traverse(this);
+
+  },
+
+  rebalance: function() {
+    var makeBalancedTree = function (arr) {
+      if (arr.length === 0) {
+        return null;
+      }
+      var index = Math.floor(arr.length/2);
+      var newTree = makeBinarySearchTree(arr[index]);
+
+      newTree.left = makeBalancedTree(arr.slice(0,index));
+      newTree.right = makeBalancedTree(arr.slice(index+1));
 
 
+      return newTree;
+    }
+
+    var arr = this.toSortedList();
+    var index = Math.floor(arr.length/2);
+    this.value = arr[index];
+    this.left = makeBalancedTree(arr.slice(0,index));
+    this.right = makeBalancedTree(arr.slice(index+1));
   },
 
   breadthFirstLog: function() {
 
   },
-
-  rebalance: function(tree) {
-    // Create a new tree
-    // Pick a node to be top of the new tree
-
-    // Add all old nodes to the new tree
+  toSortedList: function () {
+    var list = []
+    function sortNumber(a,b) {
+      return a - b;
+    }
+    var recurse = function (tree) {
+      list.push(tree.value);
+      if (tree.left) {
+        recurse(tree.left);
+      }
+      if (tree.right) {
+        recurse(tree.right);
+      }
+    }
+    recurse(this);
+    list.sort(sortNumber);
+    return list;
   },
+
+  size: function() {
+    var leftSize = this.left === null ? 0 : this.left.size();
+    var rightSize = this.right === null ? 0 : this.right.size();
+    return 1 + leftSize + rightSize;
+  },
+
+
 
   minDepth: function(tree) {
     //return Math.pow(2, tree.nodes.length) - 1;
@@ -33,10 +92,37 @@ makeBinarySearchTree.prototype = {
   },
 
   contains: function(target) {
-
+    function traverse (tree) {
+      if (tree.value === target) {
+        return true;
+      }
+      if (tree.value < target) {
+        if (tree.right === null) {
+          return false;
+        } else {
+          return traverse(tree.right);
+        }
+      } else if (tree.value > target) {
+        if (tree.left === null) {
+          return false;
+        } else {
+          return traverse(tree.left);
+        }
+      }
+    }
+    return traverse(this);
   },
   depthFirstLog: function(callback) {
-
+    function traverse(tree) {
+      callback(tree.value);
+      if (tree.right) {
+        traverse(tree.right);
+      }
+      if (tree.left) {
+        traverse(tree.left);
+      }
+    }
+    traverse(this);
   }
 };
 
