bcf345d0501ea03b341910892a97647d1c92c2fc
tgrrtt <jtgarrett123@gmail.com>
2014-09-12 21:15:06 -0700

    implemented BST++ insert method

--- sprint-two/src/binarySearchTreeExtra.js
+++ sprint-two/src/binarySearchTreeExtra.js
@@ -3,30 +3,16 @@ var makeBinarySearchTree = function(value){
   newTree.value = value;
   newTree.left = null;
   newTree.right = null;
+  newTree.parent = null;
   return newTree;
 };
 
 makeBinarySearchTree.prototype = {
   insert: function(value) {
-    function traverse (tree) {
-      if (tree.value < value) {
-        if (!tree.right) {
-          tree.right = makeBinarySearchTree(value);
-        } else {
-          traverse(tree.right);
-        }
-      } else if (tree.value > value) {
-        if (!tree.left) {
-          tree.left = makeBinarySearchTree(value);
-        } else {
-          traverse(tree.left);
-        }
-      } else {
-        return;
-      }
-    }
-    traverse(this);
+
+
   },
+
   breadthFirstLog: function() {
 
   },
@@ -34,17 +20,12 @@ makeBinarySearchTree.prototype = {
   rebalance: function(tree) {
     // Create a new tree
     // Pick a node to be top of the new tree
-    var nodes = [];
-    this.depthFirstLog(function(value) {
-      nodes.push[value];
-    });
-    nodes.sort();
-    console.log(nodes);
+
     // Add all old nodes to the new tree
   },
 
   minDepth: function(tree) {
-
+    //return Math.pow(2, tree.nodes.length) - 1;
   },
 
   maxDepth: function(tree) {
@@ -52,37 +33,10 @@ makeBinarySearchTree.prototype = {
   },
 
   contains: function(target) {
-    function traverse (tree) {
-      if (tree.value === target) {
-        return true;
-      }
-      if (tree.value < target) {
-        if (tree.right === null) {
-          return false;
-        } else {
-          return traverse(tree.right);
-        }
-      } else if (tree.value > target) {
-        if (tree.left === null) {
-          return false;
-        } else {
-          return traverse(tree.left);
-        }
-      }
-    }
-    return traverse(this);
+
   },
   depthFirstLog: function(callback) {
-    function traverse(tree) {
-      callback(tree.value);
-      if (tree.right) {
-        traverse(tree.right);
-      }
-      if (tree.left) {
-        traverse(tree.left);
-      }
-    }
-    traverse(this);
+
   }
 };
 
